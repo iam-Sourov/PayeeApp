8c5e6243e1205163442d3821e536c17b22deb65a
Sourov-Ahmed26 <silenboyk7@gmail.com>
2025-08-23 09:32:23 +0600

    Added Everything

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const nmbr = 01679714839;
+const pin = 2612;
+document.getElementById("loginbtn").addEventListener("click", function (e) {
+    e.preventDefault()
+
+    const nmbrValue = document.getElementById("nmbr").value;
+    const pinValue = document.getElementById("pin").value;
+
+    const parseNmbr = parseInt(nmbrValue);
+    const parsePin = parseInt(pinValue);
+    if (parseNmbr === nmbr && parsePin === pin) {
+        window.location.href="./main.html"
+    }
+    else {
+        alert("invalid Credentials")
+
+    }
+}) 
