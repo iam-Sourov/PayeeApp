ea3d20d148a0c294f61b7f9298d0f3061c56aa5b
Sourov-Ahmed26 <silenboyk7@gmail.com>
2025-08-23 21:15:12 +0600

    Text's Resolve

--- scripts.js
+++ scripts.js
@@ -1,18 +1,15 @@
-const nmbr = 01679714839;
-const pin = 2612;
+
 document.getElementById("loginbtn").addEventListener("click", function (e) {
     e.preventDefault()
+    const nmbr = 01679714839;
+    const pin = 2612;
+    const nmbrValue = parseInt(document.getElementById("nmbr").value);
+    const pinValue = parseInt(document.getElementById("pin").value);
 
-    const nmbrValue = document.getElementById("nmbr").value;
-    const pinValue = document.getElementById("pin").value;
-
-    const parseNmbr = parseInt(nmbrValue);
-    const parsePin = parseInt(pinValue);
-    if (parseNmbr === nmbr && parsePin === pin) {
-        window.location.href="./home.html"
+    if (nmbrValue === nmbr && pinValue === pin) {
+        window.location.href = "./home.html"
     }
     else {
         alert("invalid Credentials")
-
     }
 }) 
